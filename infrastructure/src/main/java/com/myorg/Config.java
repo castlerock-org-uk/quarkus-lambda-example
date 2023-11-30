b4c9686624fec811fee6b6969a66453a640f5683
Scott Chamberlain <scott.chamberlain@icloud.com>
2023-11-30 15:53:01 +0000

    infrastructure probably complete

--- infrastructure/src/main/java/com/myorg/Config.java
+++ infrastructure/src/main/java/com/myorg/Config.java
@@ -1,7 +1,15 @@
 package com.myorg;
 
-public interface Constants {
+public final class Config {
 
-    String FUNCTION_NAME = "testFunction";
+    private Config() {
+        //Private constructor to prevent instantiation
+    }
+
+    public static final String functionZip = "../software/lambda-example/target/function.zip";
+    public static final String quarkusFunctionHandler = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest";
+    public static final int lambdaTimeout = 10;
+
+    public static final String FUNCTION_NAME = "testFunction";
 
 }
--- infrastructure/src/main/java/com/myorg/QuarkusLambdaExampleStack.java
+++ infrastructure/src/main/java/com/myorg/QuarkusLambdaExampleStack.java
@@ -1,6 +1,17 @@
 package com.myorg;
 
+import org.jetbrains.annotations.NotNull;
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.CfnOutputProps;
 import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.apigatewayv2.alpha.AddRoutesOptions;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApi;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApiProps;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpMethod;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpRouteIntegration;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpRouteIntegrationBindOptions;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpRouteIntegrationConfig;
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpLambdaIntegration;
 import software.amazon.awscdk.services.lambda.Architecture;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
@@ -9,26 +20,44 @@ import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
+
 import java.util.Map;
+
+import static java.util.Collections.singletonList;
 
 public class QuarkusLambdaExampleStack extends Stack {
 
     public QuarkusLambdaExampleStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        //TODO: Add snapstart, does architecture than hava to be X86?
+        //TODO: Add snapstart, does architecture than have to be X86?
         //Memory size 1700 equals 1 cpu
-        var function = Function.Builder.create(this, Constants.FUNCTION_NAME)
-                .functionName(Constants.FUNCTION_NAME)
+        var function = Function.Builder.create(this, Config.FUNCTION_NAME)
+                .functionName(Config.FUNCTION_NAME)
                 .runtime(Runtime.JAVA_21)
                 .architecture(Architecture.ARM_64)
                 .memorySize(1700)
-                .code(Code.fromAsset(Defaults.functionZip))
-                .handler(Defaults.quarkusFunctionHandler)
-                .timeout(Duration.seconds(Defaults.lambdaTimeout))
+                .code(Code.fromAsset(Config.functionZip))
+                .handler(Config.quarkusFunctionHandler)
+                .timeout(Duration.seconds(Config.lambdaTimeout))
                 .environment(Map.of(
                         "JAVA_TOOL_OPTIONS", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1"))
                 .build();
-        
+
+        var httpAPI = new HttpApi(this, Config.FUNCTION_NAME + "Api", HttpApiProps.builder()
+                .apiName(Config.FUNCTION_NAME + "Api")
+                .build());
+
+        var httpLambdaIntegration = HttpLambdaIntegration.Builder.create(Config.FUNCTION_NAME + "Integration", function).build();
+
+        httpAPI.addRoutes(AddRoutesOptions.builder()
+                .path("/test")
+                .methods(singletonList(HttpMethod.GET))
+                .integration(httpLambdaIntegration)
+                .build());
+
+        new CfnOutput(this, "api-endpoint", CfnOutputProps.builder()
+                .value(httpAPI.getApiEndpoint())
+                .build());
     }
 }
